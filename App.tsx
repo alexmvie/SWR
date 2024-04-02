8569a9dab7d52ddc04c63b65edd13a4a9e2d32d6
Alex Mastny <alexandermastny@gmail.com>
2024-04-02 06:49:23 +0200

    first simple fetching

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+type Post = {
+      userId: number;
+      id: number;
+      title: string;
+      body: string;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      const { data, isLoading, error } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+
+      if (isLoading) {
+            return (
+                  <SafeAreaView style={styles.container}>
+                        <ActivityIndicator size='large' />;
+                  </SafeAreaView>
+            );
+      }
+
+      if (error) {
+            return (
+                  <SafeAreaView style={styles.container}>
+                        <Text>failed to load due to {error?.message}</Text>
+                  </SafeAreaView>
+            );
+      }
+
+      console.log(JSON.stringify(data, null, 2));
+
+      return (
+            <SafeAreaView style={styles.container}>
+                  <Text>Loaded Data:</Text>
+                  {data.map((post: Post) => (
+                        <View
+                              key={post.id}
+                              style={{ padding: 10 }}
+                        >
+                              <Text style={{ fontWeight: 'bold' }}>{post.title}</Text>
+                              <Text style={{ fontStyle: 'italic', paddingHorizontal: 10 }}>{post.body}</Text>
+                        </View>
+                  ))}
+                  <StatusBar style='auto' />
+            </SafeAreaView>
+      );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      container: {
+            flex: 1,
+            backgroundColor: '#fff',
+      },
 });
